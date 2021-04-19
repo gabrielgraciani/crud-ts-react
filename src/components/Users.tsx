7e862294daf90e270c3203af66bf3e3d6e7fd9f5
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-04-19 20:31:30 -0300

    added create and remove users

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,16 +1,54 @@
+import React, { useState } from 'react';
 import { FiEdit2, FiTrash2 } from 'react-icons/fi';
 
 import '../styles/users.scss';
 
+interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
 const User = () => {
+
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState<string>('');
+
+  const handleCreateUser = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(!name || !email) {
+      alert('Preencha ambos os campos!');
+
+      return;
+    }
+
+    const id = Math.floor(Math.random() * 1000);
+    const newUser = {
+      id,
+      name,
+      email,
+    }
+
+    setUsers([...users, newUser]);
+    setName('');
+    setEmail('');
+  }
+
+  const handleDeleteUser = (id: number) => {
+    const filteredUsers = users.filter((user) => user.id !== id);
+    setUsers(filteredUsers);
+  }
+
   return (
     <div className="container">
       <div className="head">
         <h4>Lista de Usuários</h4>
 
-        <form>
-          <input type="text" placeholder="Nome" name="name" />
-          <input type="email" placeholder="E-mail" name="email" />
+        <form onSubmit={handleCreateUser}>
+          <input type="text" placeholder="Nome" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input type="email" placeholder="E-mail" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
           <input type="submit" value="Cadastrar" />
         </form>
@@ -28,27 +66,15 @@ const User = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Gabriel</td>
-            <td>gabriel@hotail.com</td>
-            <td><button><FiEdit2 /></button></td>
-            <td><button><FiTrash2 /></button></td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Gabriel</td>
-            <td>gabriel@hotail.com</td>
-            <td><button><FiEdit2 /></button></td>
-            <td><button><FiTrash2 /></button></td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Gabriel</td>
-            <td>gabriel@hotail.com</td>
-            <td><button><FiEdit2 /></button></td>
-            <td><button><FiTrash2 /></button></td>
-          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td><button><FiEdit2 /></button></td>
+              <td><button onClick={(e) => handleDeleteUser(user.id)}><FiTrash2 /></button></td>
+            </tr>
+          ))}
         </tbody>
       </table>
 
